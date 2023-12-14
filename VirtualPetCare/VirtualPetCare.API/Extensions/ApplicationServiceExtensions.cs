5eb2820b6c43e3eb58885ab2cd7b782d1bde7709
Burak Şenses <buraksenseees@gmail.com>
2023-12-14 14:28:09 +0300

    application services extension - dbcontext

---
+++ VirtualPetCare/VirtualPetCare.API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VirtualPetCare.API.Persistence;
+
+namespace VirtualPetCare.API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddDbContext<VirtualPetCareDbContext>(options =>
+        {
+            options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+
+        return services;
+    }
+}
--- VirtualPetCare/VirtualPetCare.API/Program.cs
+++ VirtualPetCare/VirtualPetCare.API/Program.cs
@@ -1,14 +1,11 @@
-using Microsoft.EntityFrameworkCore;
-using VirtualPetCare.API.Persistence;
+using VirtualPetCare.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
